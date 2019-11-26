0da46ef029400a330b9ed25c81aecf6d0b48a700
Shinta Koyanagi <yancya@upec.jp>
2019-11-26 20:24:20 +0900

    Show a event currently being held

--- scripts/generate_current_schedules_page.rb
+++ scripts/generate_current_schedules_page.rb
@@ -3,9 +3,12 @@ require 'erb'
 require 'yaml'
 
 path = Pathname.new(__FILE__)
+
+expire_time = Time.now - 60*60*12
+
 schedules =
   YAML.load_file(path + '../../data/schedules.yml').
-    select { |s| s['date'] >= Time.now }.
+    select { |s| s['date'] >= expire_time }.
     sort_by { |s| s['date'] }
 
 current = true
